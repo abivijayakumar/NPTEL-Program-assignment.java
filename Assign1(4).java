e4372f953f48d2da8b554b5cf557888e8e2f07e8
Abinaya <abivijayakumar419@gmail.com>
2025-07-29 19:51:32 +0530

    Create Assign1(4).java

---
+++ Assign1(4).java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Hashset<String> set = new HashSet<>();
+    set.add("apple");
+    set.add("banana");
+    set.add("apple");
+    System.out.println(set);
+  }
+}
