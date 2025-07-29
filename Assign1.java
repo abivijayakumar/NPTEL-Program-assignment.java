05e9ac304e27ef57c111b43cf73899efcd15c13c
Abinaya <abivijayakumar419@gmail.com>
2025-07-29 19:47:43 +0530

    Create Assign1.java

---
+++ Assign1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    List<Number> list = new ArrayList<> ();
+    list.add(2);
+    list.add(2.0);
+    System.out.println(list);
+  }
+}
