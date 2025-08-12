9e5f06cf82d1cc95e5f32642b0b91a9dca1077b1
Abinaya <abivijayakumar419@gmail.com>
2025-08-12 21:16:30 +0530

    Create week3.question2.java

---
+++ week3.question2.java
@@ -0,0 +1,19 @@
+class access {
+  public int x;
+  private int y;
+  void cal(int a, int b){
+    x = a + 1;
+    y = b;
+  }
+  void print(){
+    System.out.println(" " + y);
+  }
+}
+public class access specifier{
+  public static void main(String args[]){
+    access obj = new access();
+    obj.cal(2,3);
+    System.out.print(obj.x);
+    obj.print();
+  }
+}
